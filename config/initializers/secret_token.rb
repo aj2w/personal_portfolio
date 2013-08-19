688c64dd7914b550f236203f2cf0be3c6b7ff9bb
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-08-19 19:52:41 -0400

    Portfolio skeleton w updated gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalPortfolio::Application.config.secret_token = '90c9b9ee626b26a9b9fdaecd80307a7b8ea9baf8b17b43bd83646a568bfecc21803e9557fc4c5e71d1f58ac4bf0811b283ce0e08a67b9d09487dc6cb5b5e39b6'
