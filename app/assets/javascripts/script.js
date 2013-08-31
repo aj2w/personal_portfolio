a5bbef0b66ea2232c8974fed86ba468c591d4bb9
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-08-31 09:44:10 -0400

    Added google analytics

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -82,6 +82,18 @@ $(document).ready(function(){
   });
 });
 
+// GOOGLE ANALYTICS
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-43648984-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+// END GOOGLE ANALYTICS
+
 /*
  * Create HTML5 elements for IE's sake
  */
